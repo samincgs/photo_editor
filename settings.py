608a91fcef7699ef076fd9831d4411aa78a5a4e0
samin <samincgs@gmail.com>
2024-08-19 17:30:38 -0400

    starting to create a photoshop clone with tkinter

---
+++ settings.py
@@ -0,0 +1,22 @@
+# storing the default values here
+ROTATE_DEFAULT = 0
+ZOOM_DEFAULT = 0
+BLUR_DEFAULT = 0
+CONTRAST_DEFAULT = 0
+BRIGHTNESS_DEFAULT = 1
+VIBRANCE_DEFAULT = 1
+GRAYSCALE_DEFAULT = False
+INVERT_DEFAULT = False
+FLIP_OPTIONS = ['None', 'X', 'Y', 'Both']
+EFFECT_OPTIONS = ['None', 'Emboss', 'Find edges', 'Contour', 'Edge Enhance']
+
+
+ 
+
+# colors
+WHITE = '#FFF'
+GREY = 'grey'
+BLUE = '#1f6aa5'
+DARK_GREY = '#4a4a4a'
+CLOSE_RED = '#8a0606'
+SLIDER_BG = '#64686b'
